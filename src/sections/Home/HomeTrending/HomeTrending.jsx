e0bfe089b72cc9ebe9398f98d062c37c894e487c
Firdovsi Rzaev <firdovsirzaev@Firdovsis-Laptop.local>
2024-10-22 00:29:15 +0400

    new commit

--- src/sections/Home/HomeTrending/HomeTrending.jsx
+++ src/sections/Home/HomeTrending/HomeTrending.jsx
@@ -1,7 +1,15 @@
 import "../HomeTrending/HomeTrending.css";
 import React, { useState, useEffect } from 'react';
+import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import MenShoe from "../../../assets/Home/HomeTrending/Men/men-shoe.jpg";
+import MenJacket from "../../../assets/Home/HomeTrending/Men/men-jacket.jpeg";
+import MenShirt from "../../../assets/Home/HomeTrending/Men/men-shirt-blue.jpg";
+import WomenRed from "../../../assets/Home/HomeTrending/Women/women-red-cloth.jpg";
+import WomenGray from "../../../assets/Home/HomeTrending/Women/women-gray-cloth.jpg";
 import MenBlueShirt from "../../../assets/Home/HomeTrending/Men/men-blue-shirt.webp";
+import WomenWhite from "../../../assets/Home/HomeTrending/Women/women-white-cloth.jpg";
 
 export default function HomeTrending() {
     const [selection, setSelection] = useState('');
@@ -18,38 +26,38 @@ export default function HomeTrending() {
             price: 70.8,
             disPrice: 120
         }, {
-            image: '',
-            name: '',
+            image: MenJacket,
+            name: 'Casual Black Men Jacket',
             price: 1.1,
             disPrice: 120
         }, {
-            image: '',
-            name: '',
+            image: MenShirt,
+            name: 'Casual Blue Men Shirt',
             price: 1.1,
             disPrice: 120
         }
     ];
     const womenCards = [
         {
-            image: MenBlueShirt,
-            name: 'women',
-            price: 50.9,
+            image: WomenWhite,
+            name: 'Cashmere Tank + Bag',
+            price: 98.00,
             disPrice: 120
         }, {
-            image: MenShoe,
-            name: 'women',
-            price: 70.8,
+            image: WomenRed,
+            name: 'Cashmere Tank + Bag',
+            price: 98.00,
             disPrice: 120
         }, {
-            image: '',
-            name: '',
-            price: 1.1,
+            image: WomenGray,
+            name: 'Cashmere Tank + Bag',
+            price: 98.00,
             disPrice: 120
         }, {
-            image: '',
-            name: '',
-            price: 1.1,
-            disPrice: 120
+            image: WomenWhite,
+            name: 'Cashmere Tank + Bag',
+            price: 98.00,
+            disPrice: 120.00
         }
     ];
     const babyCards = [
@@ -139,7 +147,22 @@ export default function HomeTrending() {
                 {trendingCards.map((item) => {
                     return (
                         <div className="trending-card-container">
-                            <img src={item.image} alt="cloth"/>
+                            <div className="trend-card-img-container">
+                                <img src={item.image} alt="cloth" />
+                                <div className="trend-card-tools">
+                                    <ul>
+                                        <li>
+                                            <ShoppingCartIcon className="trend-card-cart-tool" />
+                                        </li>
+                                        <li>
+                                            <FavoriteIcon className="trend-card-fav-tool" />
+                                        </li>
+                                        <li>
+                                            <ZoomInIcon className="trend-card-zoom-tool" />
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                             <h5>{item.name}</h5>
                             <div className="trending-card-price">
                                 <p className="trending-price">{`${item.price} $`}</p>
